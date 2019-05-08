973f0541543993c49fc576a00912ed94e9d9fbd3
Grzegorz Konarski <grzegorz.konarski86@gmail.com>
2019-05-08 21:37:02 +0200

    Fix issue with es lint

--- src/App.js
+++ src/App.js
@@ -1,26 +1,109 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import myClasses from './App.module.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Person from './Person/Person';
+
+
+class App extends Component {
+  state = {
+    persons: [
+      { id: 'qwe', name: 'Max', age: 28 },
+      { id: 'dsa', name: 'Manu', age: 29 },
+      { id: 'cxz', name: 'Stephanie', age: 26 },
+    ],
+    otherState: 'some other value',
+    showPersons: false,
+  };
+
+
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex( p => { // const holds the index 
+      return p.id === id;
+    });
+
+    const person = {
+      ...this.state.persons[personIndex]
+    };
+
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
+    this.setState( {persons: persons} );
+  }
+
+  deletePersonHandler = (personIndex) => {
+    // const persons = this.state.persons.slice();
+    const persons = [...this.state.persons];
+    persons.splice(personIndex, 1);
+    this.setState({ persons: persons });
+  }
+
+  togglePersonsHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState({ showPersons: !doesShow });
+  }
+
+  render() {
+    const style = {
+      backgroundColor: 'green',
+      color: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    };
+
+    let persons = null;
+    if (this.state.showPersons) {
+      persons = (
+        <div>
+          {this.state.persons.map((person, index) => (
+            <Person
+              click={() => this.deletePersonHandler(index)}
+              name={person.name}
+              age={person.age}
+              key={person.id}
+              changed={(event) => this.nameChangedHandler(event, person.id)} // onChange and event.target.value
+            />
+          ))}
+        </div>
+      );
+      style.backgroundColor = 'red';
+    }
+
+    let assignedClasses = [];
+    if (this.state.persons.length <= 2) {
+      assignedClasses.push(myClasses.red); 
+    }
+
+    if (this.state.persons.length <= 1) {
+      assignedClasses.push(myClasses.bold);
+    } 
+
+    return (
+      <div className={myClasses.App}>
+        <h1>Hi, I am a React App</h1>
+        <p className={assignedClasses.join(' ')}>This is really working!</p>
+        <button
+          style={style}
+          onClick={this.togglePersonsHandler}
+        >Toggle Persons
+        </button>
+        {persons}
+      </div>
+    );
+  }
 }
 
 export default App;
+
+
+// ES6 arrow functions can’t be bound to a this keyword, so it will lexically go up a scope, and use the value of this in the scope in which it was defined.
+
+
+// For performance reasons React only makes a shallow identity check when it decides if something has to be rerendered. That means React only looks whether the reference has changed, it doesn't check the content. For this reason you have to make sure on your own that the reference changes each time when the content is modified (by modifying a copy and not the original array).
+
+
+// By manipulating this.state directly you are circumventing React’s state management, which can be potentially dangerous as calling setState() afterwards may replace the mutation you made.
